893f9af3e2aca5bd34c2d9a36c061c1637ee40b2
kapustkin <a.kapustkin@gmail.com>
2021-10-02 20:21:55 +0300

    [refactoring] clean up usings

---
+++ AnswersApi/AnswersApi/Filters/ErrorHandlingFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace AnswersApi.Filters
+{
+    public class ErrorHandlingFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+        public ErrorHandlingFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ErrorHandlingFilter>();
+        }
+        
+        public override void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, $"TraceIdentifier: {context.HttpContext.TraceIdentifier}");
+            
+            context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            
+            context.ExceptionHandled = true;
+        }
+    }
+}
